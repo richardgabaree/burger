d58c7d0fd4de98ff5a2c13c38362058ac170b31e
Richard Gabaree <richard.gabaree@gmail.com>
2021-02-09 23:34:23 -0600

    added orm.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name) VALUES ('Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Bacon Double Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
