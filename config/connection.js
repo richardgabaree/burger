1708e08cead28e068833dc0e367fea81a6a1531a
Richard Gabaree <richard.gabaree@gmail.com>
2021-02-10 20:56:17 -0600

    fixes to issues

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ const connection = mysql.createConnection({
   port: 3306,
   user: 'root',
   // NOTE: Be sure to add your MySQL password here!
-  password: '753951',
-  database: 'cat_db',
+  password: 'J@hntanious1',
+  database: 'burger_db',
 });
 
 // Make connection.
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+const require = require('express');
+const router = express.Router();
+
+//import the model (burger.js) to use in database
+const burger = require("..models/burger.js");
+
+
+
